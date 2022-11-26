9a3b95d9862c806afb6b478ffd83c5ed236d097c
Alex Dewey <alexmarcdewey@gmail.com>
2022-11-25 20:33:04 -0800

    main change

--- main.py
+++ main.py
@@ -1,17 +1,7 @@
-from torch.utils.data import Dataset
-
-class UserRatingDataset(Dataset):
-
-
 def setup():
-    import pandas as pd
-    COLS = ['user_id', 'movie_id', 'rating', 'timestamp']
-    train_data = pd.read_csv("./ml-100k/u1.base", sep='\t', names=COLS).drop(columns=['timestamp']).astype(int)
-    test_data = pd.read_csv("./ml-100k/u1.test", sep='\t', names=COLS).drop(columns=['timestamp']).astype(int)
-    print(train_data)
-
-    # vector user pandas representation
-    v_u_pd =
+    # vector user pandas representation\
+    from dataset import RatingMatrixDataset
+    v_u_pd = RatingMatrixDataset(1)
 
 
 if __name__ == '__main__':
