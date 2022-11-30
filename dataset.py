7146c7a14b320af90362c3b503daa01a6cb23540
Alex Dewey <alexmarcdewey@gmail.com>
2022-11-29 20:58:03 -0800

    dataset.py finished initalization

--- dataset.py
+++ dataset.py
@@ -3,6 +3,9 @@ from torch.utils.data.dataset import T_co
 
 
 class RatingMatrixDataset(Dataset):
+
+    # Init functions as a simple standard setup preparation of data by creating the user and item matricies.
+    # For implementations that save data, saving the data past this initial init is computationally effective.
     def __init__(self, destinations):
         # Import data
         import pandas as pd
@@ -10,15 +13,14 @@ class RatingMatrixDataset(Dataset):
         data = None
         for destination in destinations:
             if data is None:
-                data = pd.read_csv(destination, sep='\t', names=COLS).drop(columns=['timestamp']).astype(int)
+                data = pd.read_csv(destination, sep='\t', names=COLS).astype(int)
             else:
-                new_data = pd.read_csv(destination, sep='\t', names=COLS).drop(columns=['timestamp']).astype(int)
+                new_data = pd.read_csv(destination, sep='\t', names=COLS).astype(int)
                 data = pd.concat([data, new_data], axis=0)
 
         # Normalize rating data
         data['rating'] = data['rating'] / 5
-
-        # print(data[data['rating'] == 1].to_string())
+        data = data.reset_index()
 
         # Create (num of users) 1x(num of movies) vectors to represent users
         import torch
@@ -26,9 +28,22 @@ class RatingMatrixDataset(Dataset):
         self.item_matrix = torch.zeros(1682, 943)
         for index, row in data.iterrows():
             self.user_matrix[int(row['user_id']) - 1][int(row['movie_id']) - 1] = row['rating']
-            self.item_matrix[int(row['movie_id'] - 1)][int(row['user_id'] - 1)] = row['rating']
+            self.item_matrix[int(row['movie_id']) - 1][int(row['user_id'] - 1)] = row['rating']
 
-        # todo: Leave one out, training consists of all ratings except for most recent
+        # Creating max value of every user rating. Normally 1, but on occasion lower.
+        self.user_max = torch.max(self.user_matrix, dim=1)
 
     def __getitem__(self, index) -> T_co:
+        pass
+
+    # Simple return length
+    def __getlen__(self):
+        return len(self.user_matrix)
+
+    # Random shuffle also takes the time to prepare the leave-one-out pandas value
+    def random_shuffle(self):
+        # todo: Leave one out, training consists of all ratings except for most recent
+        pass
+
+    def get_negatives(self):
         pass
