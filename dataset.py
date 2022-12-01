faf993bf590265c45105e6ea5b675da5aeaddc64
Alex Dewey <alexmarcdewey@gmail.com>
2022-11-30 22:20:27 -0800

    Dataset now randomly shuffles, grabs a datapoint from the data and records index of datapoint

--- dataset.py
+++ dataset.py
@@ -1,5 +1,6 @@
 from torch.utils.data import Dataset
 from torch.utils.data.dataset import T_co
+import torch
 
 
 class RatingMatrixDataset(Dataset):
@@ -23,7 +24,6 @@ class RatingMatrixDataset(Dataset):
         data = data.reset_index()
 
         # Create (num of users) 1x(num of movies) vectors to represent users
-        import torch
         self.user_matrix = torch.zeros(943, 1682)
         self.item_matrix = torch.zeros(1682, 943)
         for index, row in data.iterrows():
@@ -33,7 +33,10 @@ class RatingMatrixDataset(Dataset):
         # Creating max value of every user rating. Normally 1, but on occasion lower.
         self.user_max = torch.max(self.user_matrix, dim=1)
 
-    def __getitem__(self, index) -> T_co:
+        # For later
+        self.gt = None
+
+    def __getitem__(self, index):
         pass
 
     # Simple return length
@@ -42,8 +45,22 @@ class RatingMatrixDataset(Dataset):
 
     # Random shuffle also takes the time to prepare the leave-one-out pandas value
     def random_shuffle(self):
-        # todo: Leave one out, training consists of all ratings except for most recent
-        pass
+        # Shuffle data
+        self.user_matrix = self.user_matrix[torch.randperm(self.user_matrix.size()[0])]
 
+        # Doing leave-one-out samples and storing the ground_truth and item_index
+        # ground truth to compare answer with, item_index to not create negative of value we're trying to predict
+        # gt is rand_gt rank and then index
+        torch.set_printoptions(threshold=10000)
+        self.gt = torch.zeros(943, 2)
+        for index, row in enumerate(self.user_matrix):
+            y = torch.where(row != 0)
+            z = torch.randperm(y[0].shape[0])
+            item_idx = int(y[0][int(z[0])])
+            rand_gt = row[item_idx]
+            self.gt[index] = torch.tensor([rand_gt, item_idx])
+            row[item_idx] = 0
+
+    # For getting a subset of negative examples
     def get_negatives(self):
         pass
