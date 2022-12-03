6255da3d79338fb6db8f09768c0e1b52966220f9
Alex Dewey <alexmarcdewey@gmail.com>
2022-12-03 14:13:22 -0800

    Features are appropriately concatenated and sent through the system. All weights are also appropriately normalized

--- model.py
+++ model.py
@@ -1,19 +1,38 @@
 import torch.nn as nn
+import torch
 
 
 class JNCF(nn.Module):
 
     def __init__(self):
         super(JNCF, self).__init__()
-        self.df_user_1 = nn.Linear(943, 256)
+        self.df_user_1 = nn.Linear(1682, 256)
         self.df_user_2 = nn.Linear(256, 128)
         self.df_user_3 = nn.Linear(128, 64)
 
-        self.df_item_1 = nn.Linear(1682, 256)
+        self.df_item_1 = nn.Linear(943, 256)
         self.df_item_2 = nn.Linear(256, 128)
         self.df_item_3 = nn.Linear(128, 64)
 
-        self.dl_1 = nn.Linear(128, 8)
+        self.dl_1 = nn.Linear(128, 64)
+        self.dl_2 = nn.Linear(64, 8)
+
+        self.h = nn.Linear(8, 1, bias=False)
+
+        norm_height = 0.1
+
+        nn.init.normal_(self.df_user_1.weight, 0, norm_height)
+        nn.init.normal_(self.df_user_2.weight, 0, norm_height)
+        nn.init.normal_(self.df_user_3.weight, 0, norm_height)
+
+        nn.init.normal_(self.df_item_1.weight, 0, norm_height)
+        nn.init.normal_(self.df_item_2.weight, 0, norm_height)
+        nn.init.normal_(self.df_item_3.weight, 0, norm_height)
+
+        nn.init.normal_(self.dl_1.weight, 0, norm_height)
+        nn.init.normal_(self.dl_2.weight, 0, norm_height)
+
+        nn.init.normal_(self.h.weight, 0, norm_height)
 
     def forward(self, v_u, v_i, v_j):
         import torch.nn.functional as F
@@ -32,12 +51,17 @@ class JNCF(nn.Module):
         z_j = F.relu(self.df_item_3(v_j))
 
         # Concatenate both feature learning matrices
-        import torch
         a_ui = torch.cat((z_u, z_i), 1)
         a_uj = torch.cat((z_u, z_j), 1)
 
-        # Combined matrices
-        y_ui = F.relu(self.dl_1(a_ui))
-        y_uj = F.relu(self.dl_1(a_uj))
+        z_ui = F.relu(self.dl_1(a_ui))
+        z_ui = F.relu(self.dl_2(z_ui))
+
+        z_uj = F.relu(self.dl_1(a_uj))
+        z_uj = F.relu(self.dl_2(z_uj))
+
+        # predict
+        y_ui = F.relu(self.h(z_ui))
+        y_uj = F.relu(self.h(z_uj))
 
         return y_ui, y_uj
