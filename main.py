32a0db4a1a4488b37e3d24be732d920a6fcace26
Alex Dewey <alexmarcdewey@gmail.com>
2022-12-05 15:52:20 -0800

    HR added

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import numpy as np
 import torch.nn
 
 
-def setup():
+def JNCF():
     # hyperparameters
     batch_size = 128
     learning_rate = 0.0001
@@ -13,6 +14,7 @@ def setup():
     alpha = 0.695
 
     # vector user pandas representation
+    print("Creating training dataset.")
     from dataset import RatingMatrixDataset
     train_set = RatingMatrixDataset(["./ml-100k/u1.base",
                                      "./ml-100k/u2.base",
@@ -24,17 +26,6 @@ def setup():
     train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size,
                                                shuffle=True)
 
-    # Valid set
-    valid_set = RatingMatrixDataset(["./ml-100k/u1.test",
-                                     "./ml-100k/u2.test",
-                                     "./ml-100k/u3.test",
-                                     "./ml-100k/u4.test",
-                                     "./ml-100k/u5.test"],
-                                    neg_size=10)
-
-    valid_loader = torch.utils.data.DataLoader(valid_set, batch_size=batch_size,
-                                               shuffle=False)
-
     # Model instantiation along with optimization constraints
     from model import JNCF
     JNCF = JNCF()
@@ -43,8 +34,9 @@ def setup():
 
     point_loss_function = torch.nn.BCEWithLogitsLoss()
 
-    total_loss = [0] * num_epochs
     # Training to now occur
+    print("Training")
+    total_loss = [0] * num_epochs
     for epoch in range(num_epochs):
 
         for i, data in enumerate(train_loader, 0):
@@ -61,7 +53,7 @@ def setup():
                 # Get outputs
                 y_i, y_j = JNCF(v_u, v_i, v_j)
 
-                # Calculate loss
+                # Described loss = 21%
                 point_loss = abs(point_loss_function(y_i.reshape(y_i.shape[0]), Y_ui.reshape(Y_ui.shape[0])))
                 pair_loss = torch.mean((torch.sigmoid(y_j - y_i) + torch.sigmoid(torch.pow(y_j, 2))))
                 loss = alpha * pair_loss + (1 - alpha) * point_loss
@@ -72,16 +64,69 @@ def setup():
                 # Backpropagate
                 loss.backward()
                 optimizer.step()
-    print(total_loss)
 
+    # Code to show the training
+    print(total_loss)
     plot_loss = list()
     for tensor in total_loss:
         plot_loss.append(tensor.item())
-
     import matplotlib.pyplot as plt
     plt.plot(plot_loss)
     plt.show()
 
+    # Evaluating time
+    print("Creating evaluation dataset.")
+    test_set = RatingMatrixDataset(["./ml-100k/u1.test",
+                                    "./ml-100k/u2.test",
+                                    "./ml-100k/u3.test",
+                                    "./ml-100k/u4.test",
+                                    "./ml-100k/u5.test"],
+                                   neg_size=10)
+
+    test_loader = torch.utils.data.DataLoader(test_set, batch_size=1,
+                                              shuffle=True)
+
+    # Calculating hit rate: Leave one out, feed in all items, then see if that one out is something
+    # we recommended in the top 10 it's a hit.
+    hit = 0
+    miss = 0
+    print("Evaluating")
+    user_matrix, item_matrix = test_set.get_matricies()
+    for i, data in enumerate(test_loader, 0):
+
+        v_u, v_i, item_idx, negative_set, Y_ui = data
+
+        item_ratings = np.zeros([item_matrix.shape[0]])
+
+        for i_idx, item in enumerate(item_matrix):
+
+            # Feed in all 0s and the rated item
+            if user_matrix[i][i_idx] == 0 or i_idx == item_idx:
+
+                item = item.reshape([1, 943])
+                # We just feed in the item twice I guess?
+                y_i, _ = JNCF(v_u, item, item)
+
+                if y_i == torch.tensor(0):
+                    y_i = torch.tensor(-1)
+
+                item_ratings[i_idx] = y_i
+
+        sort_index = np.argpartition(item_ratings, -10)[-10:]
+        top_sorted = sort_index[np.argsort(item_ratings[sort_index])]
+        view_top = item_ratings[top_sorted]
+
+        if int(item_idx.item()) in top_sorted:
+            hit += 1
+        else:
+            miss += 1
+
+        if i == 100:
+            break
+
+    print("hits: " + str(hit))
+    print("miss: " + str(miss))
+
 
 if __name__ == '__main__':
-    setup()
+    JNCF()
